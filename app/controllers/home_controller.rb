07ecf57140a46ff4b8099679f154e6c929f0af5d
sousuke <suguruyoshida.0807@gmail.com>
2022-01-03 21:10:17 +0900

    add:計算機能追加
    基本的なドア（袖付き二枚引き、引き戸、ドア、高窓）のガラス寸法を計算できる計算機能の追加。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,31 @@
 class HomeController < ApplicationController
     def index
-
-    end
-
-    def calculation
-        kind = params[:kind]
-        width = params[:width]
-        mh = params[:mh]
-        
-        if kind == "袖付き2枚引き"
-            @two_door = Two_door.find_by(standard:params[:standard])
-        elsif kind == "引き戸" 
+        if request.post?
+            kind = params[:kind]
+            width = params[:width]
+            mh = params[:mh]
             
-        elsif kind == "ドア"
-
+            if kind == "袖付き2枚引き"
+                two_door = TwoDoor.find_by(standard:params[:standard])
+                w_difference = two_door.standard_w - width.to_i
+                w_difference = w_difference/3
+                mh_difference = two_door.standard_h - mh.to_i
+                @gw = two_door.g_width - w_difference
+                @gh = two_door.g_height - mh_difference
+            elsif kind == "引き戸" 
+                sliding_door = SlidingDoor.find_by(standard:params[:standard])
+                w_difference = sliding_door.standard_w - width.to_i
+                w_difference = w_difference/2
+                mh_difference = sliding_door.standard_h - mh.to_i
+                @gw = sliding_door.g_width - w_difference
+                @gh = sliding_door.g_height - mh_difference
+            elsif kind == "ドア"
+                door = Door.find_by(standard:params[:standard])
+                w_difference = door.standard_w - width.to_i
+                mh_difference = door.standard_h - mh.to_i
+                @gw = door.g_width - w_difference
+                @gh = door.g_height - mh_difference
+            end
         end
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+
+two_doors_csv = CSV.readlines("db/sizedata/two_door.csv")
+two_doors_csv.shift
+two_doors_csv.each do |info|
+    TwoDoor.create(:standard => info[1],:g_width => info [2],:g_height => info [3],:standard_w => info [4],:standard_h => info [5])
+end
+
+sliding_doors_csv = CSV.readlines("db/sizedata/sliding_door.csv")
+sliding_doors_csv.shift
+sliding_doors_csv.each do |info|
+    SlidingDoor.create(:standard => info[1],:g_width => info [2],:g_height => info [3],:standard_w => info [4],:standard_h => info [5])
+end
+
+doors_csv = CSV.readlines("db/sizedata/door.csv")
+doors_csv.shift
+doors_csv.each do |info|
+    Door.create(:standard => info[1],:g_width => info [2],:g_height => info [3],:standard_w => info [4],:standard_h => info [5])
+end
+
+high_windows_csv = CSV.readlines("db/sizedata/high_window.csv")
+high_windows_csv.shift
+high_windows_csv.each do |info|
+    HighWindow.create(:standard => info[1],:g_width => info [2],:g_height => info [3],:standard_w => info [4],:standard_h => info [5])
+end
